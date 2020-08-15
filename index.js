93af1acf5ebb6f8126d4ddf961e73558a391e422
kamula <kamulaisaac@gmail.com>
2020-08-15 08:08:27 +0300

    installation and license added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const fs = require("fs")
 const appendtofile = (name) =>{
     fs.appendFile("Readme.md",name+"\n",(err)=>{
         if(err) throw err
+        
     })
+    
 }
 
 
@@ -13,4 +15,10 @@ const title = prompt("Enter project title: ")
 appendtofile(title)
 const description = prompt("Enter project description: ")
 appendtofile(description)
+const installation = prompt("describe your project: ")
+appendtofile(installation)
+const license = prompt("Enter license: ")
+appendtofile(license)
+
+console.log("contents saved successfully")
 
